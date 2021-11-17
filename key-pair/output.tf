124c50c9601f725e7ca0ce11fcd0e1ea8a5fc00e
“sberry2020” <buckingham427@gmail.com>
2021-11-17 19:17:36 +0000

    updated

---
+++ key-pair/output.tf
@@ -0,0 +1,9 @@
+output "key_pair_region" {
+  value = "us-east-2"
+}
+output "key_name" {
+  value = aws_key_pair.ilearning-wordpress.key_name
+}
+output "key_id" {
+  value = aws_key_pair.ilearning-wordpress.key_pair_id
+}
---
+++ route53/output.tf
@@ -0,0 +1,9 @@
+output "ZONE_ID" {
+  value = aws_route53_record.blog.zone_id
+}
+output "NAME" {
+  value = aws_route53_record.blog.name
+}
+output "RECORDS" {
+  value = aws_route53_record.blog.records
+}
